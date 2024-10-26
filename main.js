678965088735d5f253fa5b650349c4a4f01c56f6
CHAITANYA MHAISKAR <89506608+chaitanyamhaiskar@users.noreply.github.com>
2024-10-26 16:07:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ fetch(apiUrl)
 
       const text = document.createElement("p");
       text.className = "card-text";
-      text.textContent = item.short_description || "*No description available*";
+      text.textContent = item.short_description || "*No Description Available*";
 
       const date = document.createElement("p");
       date.className = "card-date";
